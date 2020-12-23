9913cb47b046423828a70e37861386da79c76567
rayHrx <huang627158768@gmail.com>
2020-12-22 21:44:18 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,40 @@ import reportWebVitals from "./reportWebVitals";
 
 ////////////////// ANT Components /////////////////
 import SideMenu from "./SideMenu";
+import ProductLists from "./ProductLists";
+import prepareHeaders from "./utilities/csvHeaderToTableHeader";
+import Repo from "./repository/repository"
 
 ReactDOM.render(
   <div>
-    <SideMenu />
+    <ProductLists 
+        products = {await fetchAllProducts()}
+        columns = {prepareHeaders(["Name","Size","PriceCAD","DiscountedPriceCAD","PriceRMB","DiscountedPriceRMB","ExchangeRate", "Stock"])}
+    />
   </div>,
   document.getElementById("root")
 );
+
+async function fetchAllProducts(){
+    var repo = new Repo("AIzaSyB4WmRNMzNmBI5iERYj_Q-Bw-UpRSbBzz0", "fir-7b423.firebaseapp.com", "fir-7b423");
+    addAProduct(repo);
+
+    let docs = await repo.products.getAll();
+    return docs;
+
+}
+
+function addAProduct(repo){
+    repo.products.add("Test",{
+    name: 'Test',
+    size: 32,
+    pricecad: 100,
+    discountedpricecad: 90,
+    pricermb: 500,
+    discountedpricermb: 450,
+    stock: 100,
+  })
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/repository/factory/table.js
+++ src/repository/factory/table.js
@@ -3,22 +3,30 @@ export default class table {
   constructor(collection){
     this.collection = collection;
   }
+
   add(key,entity) {
     var doc = this.collection.doc(key)
     doc.set(entity);
   }
+
   removeByKey(key){
+
     return this.collection.doc(key).delete().then(function() {
         console.log("Document successfully deleted!");
     }).catch(function(error) {
         console.error("Error removing document: ", error);
     });
+
   }
+
   getAll(){
     return this.collection.get();
   }
+
   getByKey(key){
+
     return this.collection.doc(key);
+
   }
 
 }
